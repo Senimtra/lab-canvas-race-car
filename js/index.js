1b6e37c83d1f330c86df4ef9cc3d47a0d55cdce1
Senimtra <senimtra@hotmail.com>
2021-08-07 13:04:29 +0200

    solve iteration 1: draw the game board

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
+const canvas = document.querySelector('#canvas');
+const context = canvas.getContext('2d');
+
+const roadImg = new Image();
+roadImg.src = './images/road.png';
+
+// ######################################
+// ## Iteration 1: Draw the game board ##
+// ######################################
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    context.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
+  }
 };
