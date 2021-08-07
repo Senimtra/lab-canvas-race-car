5ff5e182621e0a0e96a7729a58c1bd8bb7af3f62
Senimtra <senimtra@hotmail.com>
2021-08-07 18:02:12 +0200

    solve iteration 3: make the car move right & left

--- js/index.js
+++ js/index.js
@@ -24,5 +24,58 @@ window.onload = () => {
     // ###############################
 
     context.drawImage(carImg, (canvas.width / 2) - 27, canvas.height - 140, 50, 101);
+    // ### New player ###
+    this.player = new Player();
+    // ### Controls object ###
+    this.controls = {
+      ArrowLeft: { pressed: false },
+      ArrowRight: { pressed: false },
+    }
+    window.addEventListener('keydown', (event) => {
+      // ### Activate keys in controller object ###
+      if (this.controls[event.code]) {
+        this.controls[event.code].pressed = true
+      }
+    });
+    window.addEventListener('keyup', (event) => {
+      // ### Deactivate keys in controller object ###
+      if (this.controls[event.code]) {
+        this.controls[event.code].pressed = false;
+      }
+    });
+    displayRefresh();
+  }
+
+  function displayRefresh() {
+    // ### Draw and refresh canvas ###
+    window.requestAnimationFrame(() => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
+      context.drawImage(carImg, this.player.x, canvas.height - 140, 50, 101);
+      displayRefresh();
+      checkBoundaries();
+      playerMove();
+    });
+  }
+
+  // ###################################################
+  // ## Iteration 3: Make the car move right and left ##
+  // ###################################################
+
+  function checkBoundaries() {
+    // ### Check for boundaries ###
+    if (this.player.x < 0) this.player.x = 0;
+    if (this.player.x > canvas.width - 50) this.player.x = canvas.width - 50;
+  }
+
+  function playerMove() {
+    // ### Move player left/right ###
+    if (this.controls['ArrowLeft'].pressed === true) this.player.x -= 2;
+    if (this.controls['ArrowRight'].pressed === true) this.player.x += 2;
   }
 };
+
+// ### Player class ###
+class Player {
+  x = (canvas.width / 2) - 27;
+}
