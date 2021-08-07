5877cc4e7bb2a2026837ea9502bee5fd87f206f4
Senimtra <senimtra@hotmail.com>
2021-08-07 16:37:01 +0200

    solve iteration 2: draw the car

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ const context = canvas.getContext('2d');
 const roadImg = new Image();
 roadImg.src = './images/road.png';
 
+const carImg = new Image();
+carImg.src = './images/car.png';
+
 // ######################################
 // ## Iteration 1: Draw the game board ##
 // ######################################
@@ -15,5 +18,11 @@ window.onload = () => {
 
   function startGame() {
     context.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
+
+    // ###############################
+    // ## Iteration 2: Draw the car ##
+    // ###############################
+
+    context.drawImage(carImg, (canvas.width / 2) - 27, canvas.height - 140, 50, 101);
   }
 };
